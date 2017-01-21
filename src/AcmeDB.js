a5f1b1cd40374ddbb13e80f8cbaf06369018d777
Eric Katz <ericpkatz@gmail.com>
2017-01-21 10:08:39 -0500

    Profs comments
    Looks great!..

--- src/AcmeDB.js
+++ src/AcmeDB.js
@@ -1,12 +1,15 @@
 export default class AcmeDB{
-    constructor(usersObj) {
+    constructor(usersObj) {//naming.. call this param users
         // access array of users and add id prop to initial users
+        //dry this out - use addUser
         this.users = usersObj.users.map((user, index) => {
             user.id = index + 1;
             return user;
         });
     };
-    addUser(newUser){
+    addUser(newUser){//naming.. just instead of newUser just name this user
+        //this could result in duplicate id
+        //find the maxId and increment
         const id = this.users.length+1;
         newUser.id = id;
         this.users.push(newUser);
@@ -17,6 +20,8 @@ export default class AcmeDB{
         }).join(', ');
     };
     findById(id){
+        //this might be fine.. is find implemented in all browsers
+        //filter might be safer.. i could be wrong on this :)
         return this.users.find(function(user){
             return user.id === id;
         });
@@ -26,7 +31,7 @@ export default class AcmeDB{
             return user.id !== id;
         });
     };
-    editUser(userObj){
+    editUser(userObj){//better name might be editedUser?
         return this.users.forEach(function(user){
             if(user.id === userObj.id) {
                 user.name = userObj.name;
